f6c3e0c8298a40dc7fd00f58b298b53c4e69c44b
diegoregino <diegoregino1994@gmail.com>
2024-08-10 11:30:09 -0500

    fix: nav fix

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,9 +1,10 @@
+import clsx from 'clsx';
 import Logo from '../Logo';
-import './Topbar.module.scss';
+import styles from './Topbar.module.scss';
 
 const Topbar = () => {
   return (
-    <nav className="background-gradient">
+    <nav className={clsx('background-gradient', styles.nav)}>
       <Logo />
       <ul>
         <li>Mi negocio</li>
